12e9e4f5cd22b7a48f3c79612f1977d4de6994e1
Gunnar Bastkowski <gunnar@bastkowski.name>
2018-08-06 16:38:01 +0200

    Switch to FreeSpec

--- integration-testing/src/test/scala/name/bastkowski/sbttricks/integrationtesting/UserServiceComponentSpec.scala
+++ integration-testing/src/test/scala/name/bastkowski/sbttricks/integrationtesting/UserServiceComponentSpec.scala
@@ -2,38 +2,40 @@ package name.bastkowski.sbttricks.integrationtesting
 
 import org.scalatest._
 
-class UserServiceComponentSpec extends FunSpec with Matchers {
+class UserServiceComponentSpec extends FreeSpec with Matchers {
 
-  describe("Without any users") {
-    val serviceWithoutUsers = new UserServiceComponentImpl with UserRepositoryComponent {
-      val userRepository = new UserRepository {
-        def authenticate(username: String, password: String): Option[User] = None
+  "The UserService" - {
+    "Without any users" - {
+      val serviceWithoutUsers = new UserServiceComponentImpl with UserRepositoryComponent {
+        val userRepository = new UserRepository {
+          def authenticate(username: String, password: String): Option[User] = None
 
-        def create(user: User): User = user
+          def create(user: User): User = user
 
-        def delete(user: User): Boolean = true
+          def delete(user: User): Boolean = true
+        }
+      }.userService
+
+      "Does not authenticate" in {
+        serviceWithoutUsers.authenticate("", "") shouldBe None
       }
-    }.userService
-
-    it("Does not authenticate") {
-      serviceWithoutUsers.authenticate("", "") shouldBe None
     }
-  }
 
-  describe("With a matching user") {
-    val gunnar = User("Gunnar", "secret")
-    val serviceWithUsers = new UserServiceComponentImpl with UserRepositoryComponent {
-      val userRepository = new UserRepository {
-        def authenticate(username: String, password: String): Option[User] = Some(gunnar)
+    "With a matching user" - {
+      val gunnar = User("Gunnar", "secret")
+      val serviceWithUsers = new UserServiceComponentImpl with UserRepositoryComponent {
+        val userRepository = new UserRepository {
+          def authenticate(username: String, password: String): Option[User] = Some(gunnar)
 
-        def create(user: User): User = user
+          def create(user: User): User = user
 
-        def delete(user: User): Boolean = true
+          def delete(user: User): Boolean = true
+        }
+      }.userService
+
+      "Does authenticate" in {
+        serviceWithUsers.authenticate("", "") shouldBe Some(gunnar)
       }
-    }.userService
-
-    it("Does authenticate") {
-      serviceWithUsers.authenticate("", "") shouldBe Some(gunnar)
     }
   }
 }
